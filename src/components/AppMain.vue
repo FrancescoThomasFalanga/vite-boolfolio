42ab78f2fc23ab7efc7e501239749f78ef06fb56
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-05-29 15:57:27 +0200

    add pagination

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,6 +6,9 @@ export default {
     data() {
         return {
             projects: [],
+
+            currentPage: 1,
+            lastPage: 0,
         }
     },
 
@@ -19,13 +22,38 @@ export default {
 
     methods: {
         getProjects() {
-            axios.get("http://127.0.0.1:8000/api/projects").then((res) => {
+            axios.get("http://127.0.0.1:8000/api/projects?page=" + this.currentPage ).then((res) => {
 
                 console.log(res.data.results);
-                this.projects = res.data.results;
+                this.projects = res.data.results.data;
+                this.lastPage = res.data.results.last_page;
 
             });
-        }
+        },
+
+        nextPage() {
+
+            if(this.lastPage != this.currentPage) {
+
+                this.currentPage++;
+
+                this.getProjects();
+
+            }
+
+        },
+
+        prevPage() {
+
+            if(this.currentPage != 1) {
+
+                this.currentPage--;
+
+                this.getProjects();
+
+            }
+
+        },
     },
 }
 
@@ -39,6 +67,14 @@ export default {
     <section>
 
         <ProjectCard v-for="project in projects" :project="project"></ProjectCard>
+
+
+        <div class="d-flex gap-5">
+
+            <button @click="prevPage" class="btn-custom">Previous Page <span></span></button>
+            <button @click="nextPage" class="btn-custom">Next Page <span></span></button>
+
+        </div>
 
     </section>
 
@@ -57,6 +93,61 @@ export default {
         gap: 70px;
         margin: 100px auto;
         max-width: 1000px;
+    }
+
+    .btn-custom {
+        position: relative;
+        display: inline-block;
+        padding: 10px 20px;
+        color: #ffffff;
+        font-size: 16px;
+        text-decoration: none;
+        text-transform: uppercase;
+        overflow: hidden;
+        transition: .5s;
+        letter-spacing: 4px;
+        border: none;
+        background-color: #050810;
+
+        &:hover {
+
+            background: #03f40f;
+            color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 5px #03f40f,
+                        0 0 25px #03f40f,
+                        0 0 50px #03f40f,
+                        0 0 100px #03f40f;
+
+        }
+
+        span {
+
+            position: absolute;
+            display: block;
+
+            &:nth-child(1) {
+
+                bottom: 2px;
+                left: -100%;
+                width: 100%;
+                height: 2px;
+                background: linear-gradient(90deg, transparent, #03f40f);
+                animation: btn-anim1 2s linear infinite;
+
+            }
+        }
+
+    }
+
+    @keyframes btn-anim1 {
+        0% {
+        left: -100%;
+        }
+    
+        50%,100% {
+        left: 100%;
+        }
     }
 
 </style>
